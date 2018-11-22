89fd7c71d315a7fdc77b6768136ed529dcdd8da3
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 06:30:30 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/KotlinSpringBootSaveImageMySQL/KotlinSpringBootSaveImageMySqlApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.KotlinSpringBootSaveImageMySQL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootSaveImageMySqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootSaveImageMySqlApplication>(*args)
+}
