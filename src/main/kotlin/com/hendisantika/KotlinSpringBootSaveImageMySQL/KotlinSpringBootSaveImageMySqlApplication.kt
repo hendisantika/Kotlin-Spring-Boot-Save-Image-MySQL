8d97e47f0b76fdb059565926fc2b59587e020c01
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 06:35:18 +0700

    #5 - Implement Client to save/retrieve files/images

--- src/main/kotlin/com/hendisantika/KotlinSpringBootSaveImageMySQL/KotlinSpringBootSaveImageMySqlApplication.kt
+++ src/main/kotlin/com/hendisantika/KotlinSpringBootSaveImageMySQL/KotlinSpringBootSaveImageMySqlApplication.kt
@@ -1,10 +1,41 @@
 package com.hendisantika.KotlinSpringBootSaveImageMySQL
 
+import com.hendisantika.KotlinSpringBootSaveImageMySQL.model.ImageModel
+import com.hendisantika.KotlinSpringBootSaveImageMySQL.repository.ImageRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @SpringBootApplication
-class KotlinSpringBootSaveImageMySqlApplication
+class KotlinSpringBootSaveImageMySqlApplication {
+    @Bean
+    fun imageProcess(imageRepository: ImageRepository) = CommandLineRunner {
+        // image 1
+        val backImgFile = ClassPathResource("image/jsa_about_img_black_background.png")
+        var arrayPic = ByteArray(backImgFile.contentLength().toInt())
+        backImgFile.inputStream.read(arrayPic)
+        val blackImage = ImageModel(1, "JSA-ABOUT-IMAGE-BLACK-BACKGROUND", "png", arrayPic)
+
+        // image 2
+        val blueImgFile = ClassPathResource("image/jsa_about_img_blue_background.png")
+        arrayPic = ByteArray(blueImgFile.contentLength().toInt())
+        blueImgFile.inputStream.read(arrayPic)
+        val blueImage = ImageModel(2, "JSA-ABOUT-IMAGE-BLUE-BACKGROUND", "png", arrayPic)
+
+        // store image to MySQL via SpringJPA
+        imageRepository.save(blackImage)
+        imageRepository.save(blueImage)
+
+        // retrieve image from MySQL via SpringJPA
+        imageRepository.findAll().forEach { imageModel ->
+            Files.write(Paths.get("retrieve-dir/" + imageModel.name + "." + imageModel.type), imageModel.pic)
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringBootSaveImageMySqlApplication>(*args)
