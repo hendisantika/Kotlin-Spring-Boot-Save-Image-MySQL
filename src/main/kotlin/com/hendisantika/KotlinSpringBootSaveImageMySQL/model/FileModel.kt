d9fdc31f853f6736d3292ad4fe4b0fa8c8aae3d7
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 06:33:20 +0700

    #3 - Add File Model Class

---
+++ src/main/kotlin/com/hendisantika/KotlinSpringBootSaveImageMySQL/model/FileModel.kt
@@ -0,0 +1,30 @@
+package com.hendisantika.KotlinSpringBootSaveImageMySQL.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Kotlin-Spring-Boot-Save-Image-MySQL
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-11-23
+ * Time: 06:32
+ */
+@Entity
+@Table(name = "image_model")
+data class ImageModel(
+        @Id
+        @Column(name = "id")
+        val id: Long,
+
+        @Column(name = "name")
+        val name: String,
+
+        @Column(name = "type")
+        val type: String,
+
+        @Lob
+        @Column(name = "pic")
+        val pic: ByteArray
+) {
+    private constructor() : this(-1, "", "", byteArrayOf())
+}
